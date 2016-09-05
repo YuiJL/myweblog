9c868727426582db99c87860f77ddacf6d982d46
Jiayi Li <flymeljy@gmail.com>
2016-09-04 21:20:31 -0700

    minor bug fixed

--- www/orm.py
+++ www/orm.py
@@ -8,16 +8,16 @@ import asyncio, aiomysql, logging
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
 
-# create a connection pool, stored by global variable '__pool'
+# create a connection pool, stored by global variable __pool
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
         host = kw.get('host', 'localhost'),
         port = kw.get('port', 3306),
-        user = kw.get('user'),
-        password = kw.get('password'),
-        db = kw.get('db'),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
         charset = kw.get('charset', 'utf-8'),
         autocommit = kw.get('autocommit', True),
         maxsize = kw.get('maxsize', 10),
@@ -30,13 +30,12 @@ async def select(sql, args, size=None):
     log(sql, args)
     global __pool
     async with __pool.get() as conn:
-        async with conn.cursor(aiomysql.DictCursor) as cur:
-            await cur.execute(sql.replace('?', '%s'), args or ())
+        async with conn.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute(sql.replace('?', '%s'), args or ()) # replace placeholder '?'(SQL) with '%s'(MySQL)
             if size:
-                rs = await cur.fetchmany(size)
+                rs = await cursor.fetchmany(size)
             else:
-                rs = yield from cur.fetchall()
-        await cur.close()
+                rs = await cursor.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
     
