9ede4a55c4091e9522db19669dd453b5b37344d4
Jiayi Li <flymeljy@gmail.com>
2016-09-04 23:03:40 -0700

    init fields

---
+++ www/fields.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jiayi Li'
+
+class Field(object):
+    pass
+
+class StringField(Field):
+    pass
+
+class BooleanField(Field):
+    pass
+
+class IntegerField(Field):
+    pass
+
+class FloatField(Field):
+    pass
+
+class TextField(Field):
+    pass
