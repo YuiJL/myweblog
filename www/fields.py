4383e65eea765015d782524b4831e750647b0e8c
Jiayi Li <flymeljy@gmail.com>
2016-09-05 17:07:37 -0700

    class Field complete

--- www/fields.py
+++ www/fields.py
@@ -4,19 +4,37 @@
 __author__ = 'Jiayi Li'
 
 class Field(object):
-    pass
+    
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+        
+    def __str(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 class StringField(Field):
-    pass
+    
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
 
 class BooleanField(Field):
-    pass
+    
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
 
 class IntegerField(Field):
-    pass
+    
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
 
 class FloatField(Field):
-    pass
+    
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
 
 class TextField(Field):
-    pass
+    
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
