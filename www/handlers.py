567da9749f595c2448337d54189af0b83557c9f2
Jiayi Li <flymeljy@gmail.com>
2016-09-07 16:33:28 -0700

    add methods

--- www/handlers.py
+++ www/handlers.py
@@ -13,10 +13,23 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
-@get('/')
-async def index(request):
+@get('/test')
+async def test():
     users = await User.findAll()
     return {
         '__template__': 'test.html',
         'users': users
+    }
+    
+@get('/')
+async def index():
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
