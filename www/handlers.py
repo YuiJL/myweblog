fe0cea1524fd3ad46f65b0186ae3b16e075022a4
Jiayi Li <flymeljy@gmail.com>
2016-09-09 20:34:44 -0700

    add sign up api

--- www/handlers.py
+++ www/handlers.py
@@ -11,6 +11,8 @@ import re, time, json, logging, hashlib, base64, asyncio
 
 from coroweb import get, post
 
+from errors import APIValueError, APIResourceNotFoundError
+
 from models import User, Comment, Blog, next_id
 
 @get('/test')
@@ -33,3 +35,41 @@ async def index():
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+@get('/register')
+async def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+@get('/api/users')
+async def api_get_users(*, name):
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+@post('/api/users')
+async def api_register(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = await User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://i1.pixiv.net/img-original/img/2016/06/26/22/41/07/57606024_p0.png')
+    await user.save()
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
