2485c4b9a2f20f1a2565a426a7af57ecc6d5f6f1
Jiayi Li <flymeljy@gmail.com>
2016-09-11 23:26:23 -0700

    add register

--- app/views/route.py
+++ app/views/route.py
@@ -3,10 +3,9 @@
 
 __author__ = 'Jiayi Li'
 
-import time
-from flask import Flask, redirect, url_for, render_template, jsonify, abort, Blueprint
-from bson.objectid import ObjectId
+from flask import Flask, request, redirect, url_for, render_template, jsonify, abort, Blueprint
 from app import db
+from app.models import User
 
 route = Blueprint('route', __name__)
 
@@ -16,10 +15,22 @@ APIS = ['blogs', 'users', 'comments']
 def index():
     return render_template('blogs.html', blogs=db.blogs.find())
 
+@route.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['sha1_password']
+        user = dict(name=name, email=email, password=password)
+        db.users.insert_one(user)
+        return redirect(url_for('route.index'))
+
 @route.route('/api/<collection>')
 def api_get(collection):
     if collection not in APIS:
-        abort(404)
+        abort(400)
     cursor = db[collection]
     a = []
     for item in cursor.find():
