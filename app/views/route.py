b1c78289147e422d3b80426951c81890ce779b4b
Jiayi Li <flymeljy@gmail.com>
2016-09-12 23:45:22 -0700

    self register & set cookie

--- app/views/route.py
+++ app/views/route.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Jiayi Li'
 
-from flask import Flask, request, redirect, url_for, render_template, jsonify, abort, Blueprint
+from flask import Flask, request, redirect, url_for, render_template, jsonify, abort, Blueprint, make_response
 from app import db
 from app.models import User
 
@@ -20,12 +20,21 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
+        users = db.users
         name = request.form['name']
+        if (users.find_one({'name': name})):
+            resp = make_response('name is taken', 400)
+            return resp
         email = request.form['email']
+        if (users.find_one({'email': email})):
+            resp = make_response('email is taken', 400)
+            return resp
         password = request.form['sha1_password']
-        user = dict(name=name, email=email, password=password)
-        db.users.insert_one(user)
-        return redirect(url_for('route.index'))
+        user = User(name=name, email=email, password=password)
+        user_resp = user.__dict__
+        user_resp['password'] = '******'
+        user_resp.update(_id=str(user_resp['_id'])) # because '_id' in a user is not a string
+        return user.signInResponse(jsonify(user=user_resp))
 
 @route.route('/api/<collection>')
 def api_get(collection):
