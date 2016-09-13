d81df85ed74befea9f2531af9de07331de814b06
Jiayi Li <flymeljy@gmail.com>
2016-09-12 22:34:15 -0700

    update ajax

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -3,7 +3,7 @@ function validEmail(email) {
     return re.test(email);
 }
 
-new Vue({
+var vm = new Vue({
     el: '#signupform',
     data: {
         name: '',
@@ -14,8 +14,9 @@ new Vue({
     },
     methods: {
         submit: function() {
-            var email = this.email.trim();
-            if (!validEmail(email)) {
+            this.email = this.email.trim();
+            var self = this;
+            if (!validEmail(this.email)) {
                 this.message = "Please enter a valid e-mail address!";
                 return $('.alert').show();
             }
@@ -27,12 +28,23 @@ new Vue({
                 this.message = "The two passwords don't match!";
                 return $('.alert').show();
             }
-            $.post('/register', {
-                name: this.name.trim(),
-                email: email,
-                sha1_password: sha1(email + ':' + this.password1)
-            }, function() {
+            $.ajax('/register', {
+                data: {
+                    name: self.name.trim(),
+                    email: self.email,
+                    sha1_password: sha1(self.email + ':' + self.password1)
+                }, 
+                method: "POST"
+            }).done(function() {
                 return location.assign('/');
+            }).fail(function(xhr) {
+                if (xhr.responseText === "name is taken") {
+                    self.message = 'Username is already in use!';
+                }
+                if (xhr.responseText === "email is taken") {
+                    self.message = 'E-mail address is already in use!';
+                }
+                return $('.alert').show();
             });
         }
     }
