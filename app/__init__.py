54652622e83b41ae787e7852506f28846d862dbc
Jiayi Li <flymeljy@gmail.com>
2016-09-14 01:22:29 -0700

    set flask.g, put into before_request

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,26 @@
 
 __author__ = 'Jiayi Li'
 
-from flask import Flask
+import time
+from flask import Flask, request, g
 from pymongo import MongoClient
 from app.filters import datetime_filter
+
+COOKIE_NAME = "YuiSession"
 
 client = MongoClient()
 db = client.test
 
+from app.utilities import cookieToUser
+from app.views.route import route
+
+def loginStatus():
+    cookie = request.cookies.get(COOKIE_NAME)
+    g.__user__ = cookieToUser(cookie)
+
 def create_app():
     app = Flask(__name__)
     app.jinja_env.filters.update(datetime=datetime_filter)
-    from app.views.route import route
+    app.before_request(loginStatus)
     app.register_blueprint(route)
     return app
