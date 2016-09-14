7808f5cce95ddff72defe80f8edea7d6ca5aef67
Jiayi Li <flymeljy@gmail.com>
2016-09-14 11:38:56 -0700

    some adjustments

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -19,42 +19,10 @@ new Vue({
                 return location.assign('/');
             }).fail(function(xhr) {
                 if (xhr.responseText === "invalid email") {
-                    self.message = 'E-mail address not found!';
+                    self.message = 'E-mail not found!';
                 }
                 if (xhr.responseText === "wrong password") {
-                    self.message = 'Password is wrong!';
-                }
-                return $('.alert').show();
-            });
-        }
-    }
-});
-
-new Vue({
-    el: '#dropdownlogin',
-    data: {
-        email: '',
-        password: '',
-        message: ''
-    },
-    methods: {
-        login: function() {
-            this.email = this.email.trim();
-            var self = this;
-            $.ajax('/auth', {
-                data: {
-                    email: self.email,
-                    sha1_password: sha1(self.email + ':' + self.password)
-                }, 
-                method: "POST"
-            }).done(function() {
-                return location.assign('/');
-            }).fail(function(xhr) {
-                if (xhr.responseText === "invalid email") {
-                    self.message = 'E-mail address not found!';
-                }
-                if (xhr.responseText === "wrong password") {
-                    self.message = 'Password is wrong!';
+                    self.message = 'Wrong password!';
                 }
                 return $('.alert').show();
             });
