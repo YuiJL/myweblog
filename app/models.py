1644aba1eed5fb202af95344aa877ca37cee8b4b
Jiayi Li <flymeljy@gmail.com>
2016-09-15 13:32:36 -0700

    add Blog class

--- app/models.py
+++ app/models.py
@@ -21,15 +21,26 @@ class User(object):
         self.register()
     
     def register(self):
-        sha1_password = self.password + self.email + 'the-Salt'
+        sha1_password = self.password + self.email + 'the-Salt' # secondary encryption
         self.password = hashlib.sha1(sha1_password.encode('utf-8')).hexdigest()
         db.users.insert_one(self.__dict__)
-        
+
+
 class Blog(object):
     
     def __init__(self, **kw):
-        pass
-    
+        self.user_id = kw.get('user_id')
+        self.user_name = kw.get('user_name')
+        self.user_image = kw.get('user_image')
+        self.title = kw.get('title')
+        self.tag = kw.get('tag', [])
+        self.content = kw.get('content')
+        self.summary = '%s%s' % (kw.get('content', '')[:140], '...')
+        self.created = kw.get('created', int(time.time()))
+        self.lastModified = False
+        db.blogs.insert_one(self.__dict__) # save to mongodb
+
+
 class Comment(object):
     
     def __init__(self, **kw):
